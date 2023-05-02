2931dea269eba00dbb7b9b60690c0c2060fb0714
Hilary Luo <hluo@clearpathrobotics.com>
2023-05-01 23:26:37 -0400

    Add Patrol Loop example

--- turtlebot4_python_tutorials/setup.py
+++ turtlebot4_python_tutorials/setup.py
@@ -26,6 +26,7 @@ setup(
             'nav_through_poses = turtlebot4_python_tutorials.nav_through_poses:main',
             'follow_waypoints = turtlebot4_python_tutorials.follow_waypoints:main',
             'create_path = turtlebot4_python_tutorials.create_path:main',
+            'patrol_loop = turtlebot4_python_tutorials.patrol_loop:main',
         ],
     },
 )
